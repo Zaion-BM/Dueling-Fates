964135f4cdae584557623e885e3f6933812c9f4f
BD4ve <balogh.david980108@gmail.com>
2021-04-24 23:35:12 +0200

    Main ready, MainProcess elkezdve

--- DuelingFates/src/com/DuelingFates/Main/Main.java
+++ DuelingFates/src/com/DuelingFates/Main/Main.java
@@ -1,7 +1,6 @@
 package com.DuelingFates.Main;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
 
@@ -12,7 +11,7 @@ public class Main {
 
         duelingFates = new JFrame("DuelingFates");                  //új Frame
 
-        //fullscreen
+        //majd fullscreen esetben
         //duelingFates.setUndecorated(true);                              //nincs keret
         //duelingFates.setExtendedState(JFrame.MAXIMIZED_BOTH);           //max Vertical & Horizontal
 
@@ -21,10 +20,11 @@ public class Main {
         duelingFates.pack();                                            //tartalomhoz igazodik a Frame mérete
         duelingFates.setResizable(true);                                //nem lehet átméretezni
         duelingFates.setVisible(true);                                  //default esetben hidden lenne
+        duelingFates.setLocationRelativeTo(null);                       //null: az ablak a képernyőn közepén
 
-        //Frame Icon beállítása
+        // Frame Icon beállítása
         try {
-            GameLogo = new ImageIcon("DuelingFates/Sources/logo_DuelingFatesDF.png");     //Frame logo betöltése
+            GameLogo = new ImageIcon("DuelingFates/Sources/logoicon_DuelingFatesDF.png");     //Frame logo betöltése
             duelingFates.setIconImage(GameLogo.getImage());             //Icon beállítása - csak Image lehet az argumentum, ezért kell getImage()
         }
         catch (Exception e){
@@ -32,4 +32,5 @@ public class Main {
         }
 
     }
+
 }
--- DuelingFates/src/com/DuelingFates/Main/MainProcess.java
+++ DuelingFates/src/com/DuelingFates/Main/MainProcess.java
@@ -1,10 +1,67 @@
 package com.DuelingFates.Main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class MainProcess extends JPanel {
+public class MainProcess extends JPanel implements Runnable{
 
+    public final int gameWidth = 1920;
+    public final int gameHeight = 1080;
+    public final int FPS = 60;                                              // 1/60 = 16.67 millisec
+    public boolean gameIsRunning = false;
+
+    BufferedImage gameWindow;
+    Graphics2D graphics;
+
+    @SuppressWarnings("CommentedOutCode")
     MainProcess(){
+        setPreferredSize(new Dimension(gameWidth, gameHeight));             //méret megadása, csak Dimension típust értelmez
+        //Mivel mindkettő true alapértelmezetten, ezért nincs szükség rá
+        //setFocusable(true);
+        //requestFocus();                                                   //alkalmazás fókuszt kap
+        startThread();
+    }
+
+    public synchronized void startThread(){                                 //synchronized: multi-threaded esetben, egyszerre 1 szál fér az objektumhoz
+        Thread threadMain = new Thread(this);
+        threadMain.start();
+    }
+
+    public void run(){                                                      //Runnable miatt automatikusan meghívódik
+        gameWindow = new BufferedImage(gameWidth,gameHeight,BufferedImage.TYPE_INT_RGB);    //a kép melyre rajzolunk
+        graphics = gameWindow.createGraphics();                                             //grafika amit kirajzolunk
+        gameIsRunning = true;                                                               //játék már fut
+
+        final long oneFrameDuration = 1000/FPS;                             //(1/60)*1000, csak a long miatt úgy 0 lesz
+
+        while (gameIsRunning){                                              //végtelen ciklus, időszámítás, sleep
+            //meghívjük a függvényeket
+            updateGame();
+            updateScreen();
+            renderScreen();
+
+            try{
+                synchronized (this) {
+                    this.wait(oneFrameDuration);                //Thread.sleep(oneFrameDuration); az éppen futó threadet megszakítja, milisec ideig
+                    //System.out.println("I'm waiting");
+                }                                               //így a warning elkerülhető
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    void updateGame(){
+
+    }
+
+    void updateScreen(){
+
+    }
+    void renderScreen(){
 
     }
 }

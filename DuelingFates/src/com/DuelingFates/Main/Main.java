1708aedf2e979be1ac3b3e144046bd209aff7bb6
BD4ve <balogh.david980108@gmail.com>
2021-04-24 21:26:22 +0200

    I hope it works for you as well v2

--- DuelingFates/src/com/DuelingFates/Main/Main.java
+++ DuelingFates/src/com/DuelingFates/Main/Main.java
@@ -1,12 +1,16 @@
 package com.DuelingFates.Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args){
 
-        JFrame duelingFates = new JFrame("DuelingFates");           //új Frame
+        JFrame duelingFates;
+        ImageIcon GameLogo;
+
+        duelingFates = new JFrame("DuelingFates");                  //új Frame
 
         //fullscreen
         //duelingFates.setUndecorated(true);                              //nincs keret
@@ -15,8 +19,17 @@ public class Main {
         duelingFates.add(new MainProcess());                            //a Frame tartalma
         duelingFates.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //kilépünk a Frame zárásával
         duelingFates.pack();                                            //tartalomhoz igazodik a Frame mérete
-        duelingFates.setResizable(false);                               //nem lehet átméretezni
+        duelingFates.setResizable(true);                                //nem lehet átméretezni
         duelingFates.setVisible(true);                                  //default esetben hidden lenne
+
+        //Frame Icon beállítása
+        try {
+            GameLogo = new ImageIcon("DuelingFates/Sources/logo_DuelingFatesDF.png");     //Frame logo betöltése
+            duelingFates.setIconImage(GameLogo.getImage());             //Icon beállítása - csak Image lehet az argumentum, ezért kell getImage()
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }

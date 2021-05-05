858bba666baacaf5f46e254e62e0eb239f97c330
Berta Máté <matt.berta@gmail.com>
2021-05-05 09:23:58 +0200

    Networking sources added. Buildable but not tested.

--- DuelingFates/src/com/DuelingFates/Networking/Client/Client.java
+++ DuelingFates/src/com/DuelingFates/Networking/Client/Client.java
@@ -1,8 +1,41 @@
 package com.DuelingFates.Networking.Client;
 
+import java.net.*;
+import java.io.*;
+
 public class Client {
+    private String hostname;
+    private int port;
+    private String userName;
 
-    //comment
+    public Client(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
 
-    //test
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            System.out.println("Connected to the chat server");
+
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+
+    }
+
+    void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    String getUserName() {
+        return this.userName;
+    }
 }
+
--- DuelingFates/src/com/DuelingFates/Networking/Client/WriteThread.java
+++ DuelingFates/src/com/DuelingFates/Networking/Client/WriteThread.java
@@ -1,4 +1,48 @@
 package com.DuelingFates.Networking.Client;
 
-public class WriteThread {
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class WriteThread extends Thread {
+    private PrintWriter writer;
+    private Socket socket;
+    private Client client;
+
+    public WriteThread(Socket socket, Client client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter your name: ");
+
+        String userName = scanner.nextLine();
+        client.setUserName(userName);
+        writer.println(userName);
+
+        String text;
+
+        do {
+            text = scanner.next();
+            writer.println(text);
+
+        } while (!text.equals("bye"));
+
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
+    }
 }
